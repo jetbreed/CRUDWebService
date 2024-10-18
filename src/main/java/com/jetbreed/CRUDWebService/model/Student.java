00f45147351020db37618818d9d1d2da231d0e28
JEBREED <info@jetbreed.com>
2024-10-18 08:26:43 +0100

    Update Student.java

--- src/main/java/com/jetbreed/CRUDWebService/model/Student.java
+++ src/main/java/com/jetbreed/CRUDWebService/model/Student.java
@@ -3,11 +3,14 @@ package com.jetbreed.CRUDWebService.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
     public class Student {
+        @Column(name = "id")
         private long id;
         private String firstname;
         private String middlename;
